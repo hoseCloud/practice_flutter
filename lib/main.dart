ca8acc4f1d6204f374403e3bea6e084e631dc65a
hosecloud <4osecloud@gmail.com>
2023-01-19 21:11:38 +0900

    Add home_screen

--- lib/main.dart
+++ lib/main.dart
@@ -1,17 +1,13 @@
 import 'package:flutter/material.dart';
+import './screens/home_screen.dart';
 
 void main() {
   runApp(const App());
 }
 
-class App extends StatefulWidget {
+class App extends StatelessWidget {
   const App({super.key});
 
-  @override
-  State<App> createState() => _AppState();
-}
-
-class _AppState extends State<App> {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -25,7 +21,7 @@ class _AppState extends State<App> {
         cardColor: const Color(0xFFF4EDDB),
       ),
       home: const Scaffold(
-        body: Center(),
+        body: HomeScreen(),
       ),
     );
   }
