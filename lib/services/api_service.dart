7a409e60e8335eaa1f558ec70a265690c98db67d
hosecloud <4osecloud@gmail.com>
2023-01-23 13:29:23 +0900

    Add api, today webtoons

---
+++ lib/services/api_service.dart
@@ -0,0 +1,28 @@
+import 'dart:convert';
+import 'package:practice_flutter/models/webtoon_model.dart';
+
+import 'package:http/http.dart' as http;
+
+class ApiService {
+  static const String baseUrl =
+      'https://webtoon-crawler.nomadcoders.workers.dev';
+  static const String today = 'today';
+
+  static Future<List<WebtoonModel>> getTodaysToons() async {
+    List<WebtoonModel> webtoonInstances = [];
+    final url = Uri.parse('$baseUrl/$today');
+    final response = await http.get(url);
+
+    if (response.statusCode == 200) {
+      final List<dynamic> webtoons = jsonDecode(response.body);
+
+      for (var webtoon in webtoons) {
+        webtoonInstances.add(WebtoonModel.fromJson(webtoon));
+      }
+    } else {
+      throw Error();
+    }
+
+    return webtoonInstances;
+  }
+}
