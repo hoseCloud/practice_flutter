e4583349f5576b1f5a57c7a7ae98c4ac51b7b1cd
hosecloud <4osecloud@gmail.com>
2023-01-24 13:08:58 +0900

    Add webtoon thumb in homeScreen

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -52,9 +52,38 @@ class HomeScreen extends StatelessWidget {
     return ListView.separated(
       scrollDirection: Axis.horizontal,
       itemCount: snapshot.data!.length,
+      padding: const EdgeInsets.symmetric(vertical: 10, horizontal: 10),
       itemBuilder: (context, index) {
         var webtoon = snapshot.data![index];
-        return Text(webtoon.title);
+        return Column(
+          children: [
+            Container(
+              width: 250,
+              clipBehavior: Clip.hardEdge,
+              decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(15),
+                  boxShadow: [
+                    BoxShadow(
+                      blurRadius: 5,
+                      offset: const Offset(10, 15),
+                      color: Colors.black.withOpacity(0.5),
+                    )
+                  ]),
+              child: Image.network(
+                webtoon.thumb,
+              ),
+            ),
+            const SizedBox(
+              height: 10,
+            ),
+            Text(
+              webtoon.title,
+              style: const TextStyle(
+                fontSize: 22,
+              ),
+            ),
+          ],
+        );
       },
       separatorBuilder: (context, index) => const SizedBox(
         width: 40,
