7b31368d8558911eda9e1e760faad25669f8efca
hosecloud <4osecloud@gmail.com>
2023-01-24 14:48:07 +0900

    Add API and model

--- lib/services/api_service.dart
+++ lib/services/api_service.dart
@@ -1,4 +1,6 @@
 import 'dart:convert';
+import 'package:practice_flutter/models/webtoon_datil_model.dart';
+import 'package:practice_flutter/models/webtoon_episode_model.dart';
 import 'package:practice_flutter/models/webtoon_model.dart';
 
 import 'package:http/http.dart' as http;
@@ -24,5 +26,36 @@ class ApiService {
     }
 
     return webtoonInstances;
+  }
+
+  static Future<WebtoonDetailModel> getToonById(String id) async {
+    final url = Uri.parse("$baseUrl/$id");
+    final response = await http.get(url);
+
+    if (response.statusCode == 200) {
+      final webtoon = jsonDecode(response.body);
+
+      return WebtoonDetailModel.fromJson(webtoon);
+    } else {
+      throw Error();
+    }
+  }
+
+  static Future<List<WebtoonEpisodeModel>> getLatestEpisodesById(
+      String id) async {
+    final url = Uri.parse("$baseUrl/$id/episodes");
+    final response = await http.get(url);
+    List<WebtoonEpisodeModel> episodesInstances = [];
+
+    if (response.statusCode == 200) {
+      final episodes = jsonDecode(response.body);
+
+      for (var episode in episodes) {
+        episodesInstances.add(WebtoonEpisodeModel.fromJson(episode));
+      }
+      return episodesInstances;
+    } else {
+      throw Error();
+    }
   }
 }
