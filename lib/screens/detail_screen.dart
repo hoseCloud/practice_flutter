6e3dd28207899c28d2f48f5f3dd8025ab52cea6d
hosecloud <4osecloud@gmail.com>
2023-01-24 14:47:48 +0900

    Add Hero in screen

--- lib/screens/detail_screen.dart
+++ lib/screens/detail_screen.dart
@@ -34,20 +34,23 @@ class DetailScreen extends StatelessWidget {
           Row(
             mainAxisAlignment: MainAxisAlignment.center,
             children: [
-              Container(
-                width: 250,
-                clipBehavior: Clip.hardEdge,
-                decoration: BoxDecoration(
-                    borderRadius: BorderRadius.circular(15),
-                    boxShadow: [
-                      BoxShadow(
-                        blurRadius: 5,
-                        offset: const Offset(10, 15),
-                        color: Colors.black.withOpacity(0.5),
-                      )
-                    ]),
-                child: Image.network(
-                  thumb,
+              Hero(
+                tag: id,
+                child: Container(
+                  width: 250,
+                  clipBehavior: Clip.hardEdge,
+                  decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(15),
+                      boxShadow: [
+                        BoxShadow(
+                          blurRadius: 5,
+                          offset: const Offset(10, 15),
+                          color: Colors.black.withOpacity(0.5),
+                        )
+                      ]),
+                  child: Image.network(
+                    thumb,
+                  ),
                 ),
               ),
             ],
